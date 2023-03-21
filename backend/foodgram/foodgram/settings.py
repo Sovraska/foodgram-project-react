2ea4f1d757e6e2734ee79ada02eab1b9c6ebe9ad
Sovraska <simon.novikov@gmail.com>
2023-03-21 18:17:17 +0300

    add allowed hosts

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-ptrp@mf%+4^!yu=sgfrrv%du&fb54c0&v8qe1nh9racd_#+kte
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['infra_backend_1:8000', '127.0.0.1', '127.0.0.1:8000' ]
+ALLOWED_HOSTS = ['infra_backend_1:8000', '127.0.0.1', '127.0.0.1:8000', '51.250.100.232']
 
 
 INSTALLED_APPS = [
