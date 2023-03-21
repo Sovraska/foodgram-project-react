6638f29c849a5422998dc9ed1eca48c987e5bbc8
Sovraska <simon.novikov@gmail.com>
2023-03-21 18:29:29 +0300

    logs for back

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -121,29 +121,57 @@ REST_FRAMEWORK = {
 
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-        }
+'version': 1,
+'disable_existing_loggers': False,
+'filters': {
+    'require_debug_false': {
+        '()': 'django.utils.log.RequireDebugFalse',
     },
-    'handlers': {
-        'gunicorn': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
-            'maxBytes': 1024 * 1024 * 100,  # 100 mb
-        }
+    'require_debug_true': {
+        '()': 'django.utils.log.RequireDebugTrue',
     },
-    'loggers': {
-        'gunicorn.errors': {
-            'level': 'DEBUG',
-            'handlers': ['gunicorn'],
-            'propagate': True,
-        },
+},
+'formatters': {
+    'django.server': {
+        '()': 'django.utils.log.ServerFormatter',
+        'format': '[%(server_time)s] %(message)s',
     }
+},
+'handlers': {
+    'console': {
+        'level': 'INFO',
+        'filters': ['require_debug_true'],
+        'class': 'logging.StreamHandler',
+    },
+    # Custom handler which we will use with logger 'django'.
+    # We want errors/warnings to be logged when DEBUG=False
+    'console_on_not_debug': {
+        'level': 'WARNING',
+        'filters': ['require_debug_false'],
+        'class': 'logging.StreamHandler',
+    },
+    'django.server': {
+        'level': 'INFO',
+        'class': 'logging.StreamHandler',
+        'formatter': 'django.server',
+    },
+    'mail_admins': {
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+        'class': 'django.utils.log.AdminEmailHandler'
+    }
+},
+'loggers': {
+    'django': {
+        'handlers': ['console', 'mail_admins', 'console_on_not_debug'],
+        'level': 'INFO',
+    },
+    'django.server': {
+        'handlers': ['django.server'],
+        'level': 'INFO',
+        'propagate': False,
+    },
+}
 }
 
 
