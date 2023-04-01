ecda9eba0542d49832cf3878bc1e2b24c761b734
Sovraska <simon.novikov@gmail.com>
2023-04-01 23:11:44 +0300

    incer in to base fix

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,59 +1,9 @@
-import json
-import os
-from contextlib import closing
-from pathlib import Path
-
-import psycopg2
 from django.shortcuts import get_object_or_404
-from dotenv import load_dotenv
-from psycopg2 import Error
-import logging
 
 from rest_framework import status
 from rest_framework.response import Response
 
 from recipes.models import RecipesModel
-
-
-PROJECT_BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-load_dotenv(os.path.join(PROJECT_BASE_DIR / 'infra', '.env'))
-
-DB_NAME = str(os.getenv('DB_NAME'))
-POSTGRES_USER = str(os.getenv('POSTGRES_USER'))
-POSTGRES_PASSWORD = str(os.getenv('POSTGRES_PASSWORD'))
-DB_HOST = str(os.getenv('DB_HOST'))
-DB_PORT = str(os.getenv('DB_PORT'))
-
-
-def insert_into_base_ingredients():
-    try:
-        with closing(psycopg2.connect(
-                dbname=DB_NAME,
-                user=POSTGRES_USER,
-                password=POSTGRES_PASSWORD,
-                host=DB_HOST,
-                port=DB_PORT
-        )) as conn:
-            with conn.cursor() as cursor:
-                with open(
-                        './data/ingredients.json',
-                        'r',
-                        encoding='utf8'
-                ) as json_file:
-                    data = json.load(json_file)
-                    for line in data:
-                        title = line.get('name')
-                        measurement_unit = line.get('measurement_unit')
-                        cursor.execute(
-                            f"INSERT INTO recipes_ingredientsmodel("
-                            f"name, measurement_unit"
-                            f") VALUES ('{title}', '{measurement_unit}');")
-                        conn.commit()
-        logging.info("Соединение с PostgreSQL закрыто")
-
-    except (Exception, Error) as error:
-        logging.error("Ошибка при работе с PostgreSQL", error)
 
 
 def delete_obj(request, pk, model):
@@ -82,7 +32,3 @@ def post_obj(request, pk, model, serializer):
     model.objects.get_or_create(user=request.user, recipe=recipe)
     data = serializer(recipe).data
     return Response(data, status=status.HTTP_201_CREATED)
-
-
-if __name__ == "__main__":
-    insert_into_base_ingredients()
